01c17eb2292e5e23ae1d877690f2f03fb587b4b5
Rafael Costa <rafaelcost@gmail.com>
2026-09-02 11:58:07 -0300

    atualização 02/09/26

--- src/main/java/com/senai/cadastro/domain/entity/Usuario.java
+++ src/main/java/com/senai/cadastro/domain/entity/Usuario.java
@@ -1,9 +1,8 @@
-package com.senai.cadastro;
+package com.senai.cadastro.domain.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -35,6 +34,11 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
+
+    @NotBlank(message = "Senha é obrigatório")
+    @Size(min = 4, max = 8, message = "Senha deve ter entre 4 e 8 caracteres")
+    @Column(name = "senha", nullable = false, length = 8)
+    private String senha;
 
     @NotBlank(message = "Nome é obrigatório")
     @Size(min = 2, max = 150, message = "Nome deve ter entre 2 e 150 caracteres")
---
+++ src/main/java/com/senai/cadastro/domain/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.senai.cadastro.domain.repository;
+
+import com.senai.cadastro.domain.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    Usuario findByEmail(String email);
+
+    Boolean existsByCpf(String cpf);
+}
